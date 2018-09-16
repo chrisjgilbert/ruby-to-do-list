ea73d7dd270be066ef43151d63ad9538920f58a8
Christopher Gilbert <chrisgilbert4@icloud.com>
2018-09-16 15:54:10 +0100

    final round of updates

--- todo.rb
+++ todo.rb
@@ -3,12 +3,10 @@
 module Menu
 	
 	def menu
-		"What would you like to do? A) Add, S) Show, D) Delete, W) Write, R) Read or Q) Quit"
+		"What would you like to do? A) Add, U) Update, S) Show, D) Delete, W) Write, R) Read, T) Toggle status, or Q) Quit"
 	end
 
 end
-
-
 
 # Promptable Module
 module Promptable
@@ -20,7 +18,6 @@ module Promptable
 	end
 
 end
-
 
 # =======CLASSES================== #
 # List Class
@@ -43,33 +40,65 @@ class List
 	end
 
 	def write_to_file(filename)
-		IO.write(filename, @all_tasks.map(&:to_s).join("\n"))
+		machinified = @all_tasks.map(&:to_machine).join("\n")
+		IO.write(filename, machinified)
 	end
 
-	def read_from_file(filename)
-		IO.readlines(filename).each do |line|
-			add(Task.new(line.chomp))
-		end
-	end
+    def read_from_file(filename)
+      IO.readlines(filename).each do |line|
+        status, *description = line.split(':')
+        status = status.include?('X')
+        add(Task.new(description.join(':').strip, status))
+      end
+    end
 
 	def delete(task_number)
 		@all_tasks.delete_at(task_number-1)
 	end
 
-end	
+	def update(task_number, task)
+		@all_tasks[task_number-1] = task
+	end
 
+	def toggle(task_number)
+		@all_tasks[task_number-1].toggle_status
+	end
+
+end	
 
 # Task Class
 class Task
 
 	attr_reader :description
+	attr_accessor :status
 
-	def initialize(description)
+	def initialize(description, status=false)
 		@description = description
+		@status = status
 	end
 
 	def to_s
 		@description
+	end
+
+	def completed?
+		@status
+	end
+
+	private
+
+	def represent_satus
+		@status == true ? "Completed: [X]" : "Completed: [ ]"
+	end
+
+	public
+
+	def to_machine
+		"#{completed? ? '[X]' : '[ ]'}" 
+	end
+
+	def toggle_status
+		@completed_status = !completed?
 	end
 
 end
@@ -84,6 +113,11 @@ if __FILE__ == $PROGRAM_NAME
 		case user_input
 		when 'a'
 			list.add(Task.new(prompt('What is the task would you like to accomplish?')))
+		when 'u'
+			puts list.show
+			list.update(prompt('Which tasks would you like to update?').to_i, Task.new(prompt('What is your new task?')))
+			puts "Updated List:"
+			puts list.show
 		when 's'
 			puts "Current To Do List:"
 			puts list.show
@@ -101,6 +135,10 @@ if __FILE__ == $PROGRAM_NAME
 			rescue
 				puts "That file was not recognized!"
 			end
+		when 't'
+			puts list.show
+			list.toggle(prompt('Which taks would you like to toggle the status for?').to_i)
+			puts list
 		else
 			puts "Sorry I didn't catch that"
 		end
